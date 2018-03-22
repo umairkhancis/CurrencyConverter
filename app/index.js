fde5b969ad04442bafcef4f731fb729e57aaad01
umair.khan <umair.khan>
2018-03-22 20:35:14 +0400

    added Logo component in the home screen

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import Home from './screens/Home';
 
 EStyleSheet.build({
-  $primaryBlue: '#4f6d7a'
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF'
 });
 
 export default () => <Home />
