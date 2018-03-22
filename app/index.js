18140741ce244a68cdf368df18cef82ba273e98b
umair.khan <umair.khan>
2018-03-22 18:37:52 +0400

    added container component with background color in home screen

--- app/index.js
+++ app/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from "react-native-extended-stylesheet";
+import Home from './screens/Home';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app/index.js!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+EStyleSheet.build({
+  $primaryBlue: '#4f6d7a'
 });
+
+export default () => <Home />
